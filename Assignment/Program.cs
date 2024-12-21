d1d7fba575dee3fc194d973475fddc2678c137ae
MoGa <MoGa@DESKTOP-HSMA4DI>
2024-12-22 01:33:20 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,12 +6,25 @@
         {
             #region Q1
 
+            //Console.WriteLine("Enter Number");
+            //int Inter = int.Parse(Console.ReadLine());
+
+            //for(int i = 1; i <= Inter; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region Q2
+
+
             Console.WriteLine("Enter Number");
             int Inter = int.Parse(Console.ReadLine());
 
-            for(int i = 1; i <= Inter; i++)
+            for (int i = 1; i <= 12; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(Inter * i);
             }
 
             #endregion
