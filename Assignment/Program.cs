0e59a6ded4c1531dcaa9c3753e76eb7503470770
MoGa <MoGa@DESKTOP-HSMA4DI>
2024-12-22 19:12:17 +0200

    video 01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Video 01
+            #region loop Statments
+
+            Console.WriteLine("1 T");
+            Console.WriteLine("2 T");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} T");
+            }
+
+            #endregion
+            #endregion
         }
     }
 }
